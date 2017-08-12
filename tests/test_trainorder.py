576a1c1696783988413f3cad27e5d3e40108eb28
James L. Key <james@bluepenguinsolutions.com>
2017-08-11 21:57:03 -0400

    Refactored variable names to remove 'Code Smell' in 'Work order' and renamed it 'Train order' to reflect purpose.

---
+++ tests/test_trainorder.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Comments go here!!!
+
+:program: CERMMorse
+:file: test_workorder
+:platform: Cross-Platform
+:synopsis: Change this text.
+
+.. moduleauthor:: James L. Key <james@bluepenguinslutions.com>
+
+.. py:currentmodule:: Unittests
+
+"""
+
+from MorseAppData import Trainorder
+from unittest import TestCase
+
+
+class TestWorkorder(TestCase):
+    def test_getworkorder(self):
+        self.fail()
+
+    def test_numworkorders(self):
+        self.fail()
